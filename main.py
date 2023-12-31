0316125574e1d62997533f770a2aae9553c27460
Dln <prod408@yandex.ru>
2023-12-31 11:12:27 +0400

    test commit from my laptop

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from tkinter import *
 root = Tk()
 root.geometry('600x800')
 root.title('Assistant bot')
+main_label = Label(text='Hello')
 btn = Button(root, text='Click me')
 btn.pack()
 root.mainloop()
-
-
-
-
-
-
